b996a230bf73b6790e501f4ff379ed9575f51b27
Erik <erikdesjardins@users.noreply.github.com>
2017-03-11 15:22:59 -0500

    firefox 53 for storage.sync

--- src/js/util/storage.js
+++ src/js/util/storage.js
@@ -1,13 +1,13 @@
 import { apiToPromise } from './promise';
 
-const _get = apiToPromise((keys, callback) => chrome.storage.local.get(keys, callback));
+const _get = apiToPromise((keys, callback) => chrome.storage.sync.get(keys, callback));
 
 export async function get(key, defaultVal = null) {
 	const { [key]: val } = await _get({ [key]: defaultVal });
 	return val;
 }
 
-const _set = apiToPromise((items, callback) => chrome.storage.local.set(items, callback));
+const _set = apiToPromise((items, callback) => chrome.storage.sync.set(items, callback));
 
 export async function set(key, val) {
 	await _set({ [key]: val });
