160a2c884be1fb0bd5c595741784097f77d9f429
Erik <erikdesjardins@users.noreply.github.com>
2017-03-11 15:33:57 -0500

    firefox deployment

--- deploy.js
+++ deploy.js
@@ -4,17 +4,32 @@
 
 const fs = require('fs');
 const path = require('path');
-const deploy = require('chrome-extension-deploy');
+const deployChrome = require('chrome-extension-deploy');
+const deployFirefox = require('firefox-extension-deploy');
+const manifest = require('./dist/manifest.json');
 
-deploy({
+deployChrome({
 	clientId: process.env.CHROME_CLIENT_ID,
 	clientSecret: process.env.CHROME_CLIENT_SECRET,
 	refreshToken: process.env.CHROME_REFRESH_TOKEN,
 	id: 'INSERT_ID_HERE',
-	zip: fs.readFileSync(path.join(__dirname, 'dist/no-emoji.zip'))
+	zip: fs.createReadStream(path.join(__dirname, 'dist/no-emoji.zip'))
 }).then(function() {
-	console.log('Deploy complete!');
+	console.log('Chrome deploy complete!');
 }, function(err) {
-	console.error(err);
+	console.error('Chrome failed:', err);
+	process.exitCode = 1;
+});
+
+deployFirefox({
+	issuer: process.env.FIREFOX_ISSUER,
+	secret: process.env.FIREFOX_SECRET,
+	id: manifest.applications.gecko.id,
+	version: manifest.version,
+	src: fs.createReadStream(path.join(__dirname, 'dist/no-emoji.zip'))
+}).then(function() {
+	console.log('Firefox deploy complete!');
+}, function(err) {
+	console.error('Firefox failed:', err);
 	process.exitCode = 1;
 });
