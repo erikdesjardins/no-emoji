4996d680453ef19671662a57a49339e8544df289
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2024-01-29 17:18:43 -0500

    update to manifest v3

--- src/js/background.entry.js
+++ src/js/background.entry.js
@@ -6,39 +6,57 @@ import { STORAGE_EXCLUDED_DOMAINS } from './util/constants';
 import { locationToRootDomain } from './util/location';
 import { get, set } from './util/storage';
 
-chrome.runtime.onMessage.addListener((enabling, { tab: { id: tabId } }, sendResponse) => {
+chrome.runtime.onMessage.addListener((enabled, { tab: { id: tabId } }, sendResponse) => {
 	// content script telling us that it ran on this page
-	if (enabling) {
-		enablePageAction(tabId);
+	if (enabled) {
+		showActionEnabled(tabId);
 	} else {
-		disablePageAction(tabId);
+		showActionDisabled(tabId);
 	}
 	sendResponse();
 });
 
-chrome.pageAction.onClicked.addListener(async ({ id: tabId, url }) => {
-	const excludedDomains = await get(STORAGE_EXCLUDED_DOMAINS, []);
+chrome.action.onClicked.addListener(async ({ id: tabId, url }) => {
 	const domain = locationToRootDomain(url);
+
+	// We can't await this here, because any promise chaining will break the "user intraction" context...
+	const alreadyHadPermission = chrome.permissions.contains({ origins: [url] });
+	try {
+		// ...which is required for this call to work.
+		await chrome.permissions.request({ origins: [url] });
+	} catch (e) {
+		console.error('Failed to request permissions for tab:', e);
+	}
+
+	if (!await alreadyHadPermission) {
+		// We just retrieved the permission, so don't toggle the activation state of the extension.
+		// This will happen e.g. on Firefox after the first click on the action for a domain,
+		// since Firefox forces all extensions to request domain permissions dynamically in MV3.
+		console.log(`First time requesting permissions for ${domain}, not toggling activation state.`);
+		return;
+	}
+
+	const excludedDomains = await get(STORAGE_EXCLUDED_DOMAINS, []);
 
 	if (excludedDomains.includes(domain)) {
 		// enabling on this domain
 		await set(STORAGE_EXCLUDED_DOMAINS, excludedDomains.filter(x => x !== domain));
-		enablePageAction(tabId);
+		showActionEnabled(tabId);
+		console.log(`Enabling the extension on ${domain}.`);
 	} else {
 		// disabling on this domain
 		await set(STORAGE_EXCLUDED_DOMAINS, [...excludedDomains, domain]);
-		disablePageAction(tabId);
+		showActionDisabled(tabId);
+		console.log(`Disabling the extension on ${domain}.`);
 	}
 });
 
-function enablePageAction(tabId) {
-	chrome.pageAction.show(tabId);
-	chrome.pageAction.setTitle({ tabId, title: chrome.i18n.getMessage('stopRemovingEmoji') });
-	chrome.pageAction.setIcon({ tabId, path: { 19: icon19, 38: icon38 } });
+function showActionEnabled(tabId) {
+	chrome.action.setTitle({ tabId, title: chrome.i18n.getMessage('stopRemovingEmoji') });
+	chrome.action.setIcon({ tabId, path: { 19: icon19, 38: icon38 } });
 }
 
-function disablePageAction(tabId) {
-	chrome.pageAction.show(tabId);
-	chrome.pageAction.setTitle({ tabId, title: chrome.i18n.getMessage('startRemovingEmoji') });
-	chrome.pageAction.setIcon({ tabId, path: { 19: iconDisabled19, 38: iconDisabled38 } });
+function showActionDisabled(tabId) {
+	chrome.action.setTitle({ tabId, title: chrome.i18n.getMessage('startRemovingEmoji') });
+	chrome.action.setIcon({ tabId, path: { 19: iconDisabled19, 38: iconDisabled38 } });
 }
--- src/js/util/messaging.js
+++
@@ -1,7 +0,0 @@
-import { apiToPromise } from './promise';
-
-const _send = apiToPromise(chrome.runtime.sendMessage);
-
-export function send(msg) {
-	return _send(msg);
-}
